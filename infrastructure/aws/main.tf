525f99d7b3a7c17ba6f69498bda44577bde5f283
agustin.celentano <agustin.celentano@nullplatform.com>
2026-07-03 15:35:18 -0300

    refactor: rename s3 iam module to s3
    Follow the tofu-modules rename of infrastructure/aws/iam/s3-assets to
    infrastructure/aws/iam/s3:
    
    - module instance s3_assets_iam -> s3_iam
    - source path iam/s3-assets -> iam/s3 (ref v6.0.0)
    - input assets_bucket -> bucket

--- infrastructure/aws/main.tf
+++ infrastructure/aws/main.tf
@@ -166,12 +166,12 @@ module "ecr_iam" {
   build_workflow_group_name = module.ci_build_workflow_user.group_name
 }
 
-module "s3_assets_iam" {
-  source = "git::https://github.com/nullplatform/tofu-modules.git//infrastructure/aws/iam/s3-assets?ref=v5.3.1"
+module "s3_iam" {
+  source = "git::https://github.com/nullplatform/tofu-modules.git//infrastructure/aws/iam/s3?ref=v6.0.0"
 
   cluster_name              = module.eks.eks_cluster_name
   build_workflow_group_name = module.ci_build_workflow_user.group_name
-  assets_bucket             = "lambda-files-aws-services"
+  bucket                    = "lambda-files-aws-services"
 }
 
 ###############################################################################
